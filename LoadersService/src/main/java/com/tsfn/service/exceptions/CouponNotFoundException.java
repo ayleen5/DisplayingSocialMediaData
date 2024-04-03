7fd6bc8fb7ffa10717ec57759bf7169811e23958
Nada Houshan <Nada Houshan@10.0.0.22>
2024-04-03 22:08:50 +0300

    instagram CRUD service

--- LoadersService/src/main/java/com/tsfn/service/exceptions/CouponNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tsfn.service.exceptions;
-
-public class CouponNotFoundException extends RuntimeException {
-	 public CouponNotFoundException(String message) {
-	        super(message);
-	    }
-}

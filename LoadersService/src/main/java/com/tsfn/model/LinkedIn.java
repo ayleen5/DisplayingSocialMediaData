37d21c5f751ef312a9b45a96babd681a0960144b
ayleen5 <ayleenm55@gmail.com>
2024-04-09 15:54:41 +0300

    linkedIn CRUD

---
+++ LoadersService/src/main/java/com/tsfn/model/LinkedIn.java
@@ -0,0 +1,53 @@
+package com.tsfn.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "linkedIn")
+public class LinkedIn {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int postId;
+
+    private LocalDateTime timestamp;
+    private String postType;
+    private int impressions;
+    private int reach;
+    private int totalClicks;
+    private int reactions;
+    private int comments;
+    private int shares;
+    
+    @Column(name = "ctr")
+    private double ctr;
+    
+    @Column(name = "engagement_rate")
+    private double engagementRate;
+    
+    @PrePersist
+    public void calculateMetrics() {
+        if (impressions > 0) {
+            ctr = (double) totalClicks / impressions;
+        } else {
+            ctr = 0.0;
+        }
+        
+        if (reach > 0) {
+            engagementRate = (double) (reactions + comments + shares) / reach;
+        } else {
+            engagementRate = 0.0;
+        }
+    }
+
+
+}
---
+++ LoadersService/src/main/java/com/tsfn/repository/LinkedInRepo.java
@@ -0,0 +1,8 @@
+package com.tsfn.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tsfn.model.LinkedIn;
+
+public interface LinkedInRepo extends JpaRepository<LinkedIn, Integer>{
+
+}

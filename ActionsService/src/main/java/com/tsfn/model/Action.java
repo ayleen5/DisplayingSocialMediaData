e5254fafbcb852719fe6ba41633676449bc0f9c3
asaad <asaad.822001@hotmail.com>
2024-04-22 17:21:46 +0300

    finish task-7 Actionsservice: CRUD ACTION DEFINITIONS

--- ActionsService/src/main/java/com/tsfn/model/Action.java
+++ ActionsService/src/main/java/com/tsfn/model/Action.java
@@ -1,10 +1,9 @@
 package com.tsfn.model;
 
 import java.sql.Time;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.Date;
+import java.util.Hashtable;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,30 +14,26 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-
 @Entity
 @Table(name= "actions")
 @Data
 public class Action {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id ;
-	
-	private String name;
-	private Date created_date; // should check if it it utils or sql
-	// need to thing about that, kinda weird
-	private String created_by;
-	private Hashtable<String,ArrayList<String>> conditions;
-	@Enumerated(EnumType.STRING)
-	private ActionsType action;
-	private Time run_on_time;
-	private int run_on_day;
-	private String notification_text;
-	private String to;
-	private boolean is_deleted;
-	private Date last_update;
-	private Date next_run;
-	
-	
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String account_id;
+    private String name;
+    private Date created_date; 
+    private String created_by;
+    private Hashtable<String,ArrayList<String>> conditions;
+    @Enumerated(EnumType.STRING)
+    private ActionsType action_type;
+    private Time run_on_time;
+    private int run_on_day;
+    private String notification_text;
+    private String recipient; 
+    private boolean isEnable;
+    private boolean is_deleted;
+    private Date last_update;
+    private Date last_run;
 }
---
+++ ActionsService/src/main/java/com/tsfn/repository/ActionRepository.java
@@ -0,0 +1,10 @@
+package com.tsfn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tsfn.model.Action;
+
+
+public interface ActionRepository extends JpaRepository<Action, Integer> {
+
+}

062406b560eb7bcba9e41743164fca0de8f22841
Ayoub Nasrallah <60236795+ayobna@users.noreply.github.com>
2024-04-29 00:46:01 +0300

    Delete SecurityService directory

--- SecurityService/src/main/java/com/tsfn/sec/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.tsfn.sec.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    UserDetailsService userDetailsService();
-}

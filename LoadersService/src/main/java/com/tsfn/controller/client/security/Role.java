83da165344c41d02177d544fc63ca3054d174d8a
ayobn <ayobnas@gmail.com>
2024-05-02 17:35:11 +0300

    task-16 add com.tsfn.controller.client.security to loader service

---
+++ LoadersService/src/main/java/com/tsfn/controller/client/security/Role.java
@@ -0,0 +1,15 @@
+package com.tsfn.controller.client.security;
+
+public enum Role {
+    CREATE_ACTION,
+    READ_ACTION,
+    UPDATE_ACTION,
+    DELETE_ACTION,
+    CREATE_METRIC,
+    READ_METRIC,
+    UPDATE_METRIC,
+    DELETE_METRIC,
+    TRIGGER_MANUAL_SCAN,
+    ADMIN;
+}
+

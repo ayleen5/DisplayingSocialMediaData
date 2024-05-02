8c736b0952c6ca1b5bca26c2356b130486343896
ayobn <ayobnas@gmail.com>
2024-05-02 17:31:33 +0300

    task-16 add new role and add com.tsfn.controller.client.security

--- SecurityMs/src/main/java/com/tsfn/sec/model/Role.java
+++ SecurityMs/src/main/java/com/tsfn/sec/model/Role.java
@@ -2,9 +2,11 @@ package com.tsfn.sec.model;
 
 public enum Role {
     CREATE_ACTION,
+    READ_ACTION,
     UPDATE_ACTION,
     DELETE_ACTION,
     CREATE_METRIC,
+    READ_METRIC,
     UPDATE_METRIC,
     DELETE_METRIC,
     TRIGGER_MANUAL_SCAN,
